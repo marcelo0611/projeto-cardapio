e0ea523c1c463ec7b82dbe050c2c782625f1a963
marcelohenrique <mh0864622@gmail.com>
2026-07-29 15:14:49 -0300

    Criação da pasta backend e frontend

---
+++ backend/projeto-cardapio/src/main/java/com/cardapio/ProjetoCardapioApplication.java
@@ -0,0 +1,13 @@
+package com.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCardapioApplication.class, args);
+	}
+
+}
