80c81b501c759d988a4633ee121e9f1954c38583
marcelohenrique <mh0864622@gmail.com>
2026-07-31 07:10:14 -0300

    Inicio entidade Prato

--- backend/projeto-cardapio/src/main/java/com/cardapio/entities/Prato.java
+++ backend/projeto-cardapio/src/main/java/com/cardapio/entities/Prato.java
@@ -1,5 +1,13 @@
 package com.cardapio.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_prato")
 public class Prato {
+	
+	@Id
+	@GeneratedValue(strategy = Generated)
 
 }
